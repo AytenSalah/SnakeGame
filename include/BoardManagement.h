a7bfb15b50b75b2acf0003cdf4bb548b4fca8ae1
AytenSalah <ayten.salah22@gmail.com>
2026-04-22 02:25:57 +0200

    Add BoardManagement class for game board handling

---
+++ include/BoardManagement.h
@@ -0,0 +1,21 @@
+#ifndef BOARDMANAGEMENT_H
+#define BOARDMANAGEMENT_H
+#include<list>
+#include"Snake.h"
+class BoardManagement
+{
+private:
+    const int WIDTH = 20;
+    const int HEIGHT = 10;
+    void drawHorizontalBorder();
+    char renderHelper(int x, int y, const std::list<Position>& snake, Position food);
+public:
+        BoardManagement();
+        void renderBoard(const std::list<Position>& snake, Position food, int score);
+        bool borderCollision(const std::list<Position>& snake);
+        int getWidth() { return WIDTH; }
+        int getHeight() { return HEIGHT; }
+};
+
+
+#endif // BOARDMANAGEMENT_H
