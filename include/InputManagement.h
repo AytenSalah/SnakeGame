c9fe31bccd2b9e680ec34700bf22e5ba52a49413
AytenSalah <ayten.salah22@gmail.com>
2026-04-22 02:19:56 +0200

    Add InputManagement header with Direction struct

---
+++ include/InputManagement.h
@@ -0,0 +1,18 @@
+#ifndef INPUTMANAGEMENT_H
+#define INPUTMANAGEMENT_H
+
+struct Direction
+{
+    int dx;
+    int dy;
+};
+
+class InputManagement
+{
+public:
+        Direction getInput();
+
+
+};
+
+#endif // INPUTMANAGEMENT_H
