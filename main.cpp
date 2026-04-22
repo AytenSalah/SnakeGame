1724da22392fb16d5f83b3fdd7f2721fd4313921
AytenSalah <ayten.salah22@gmail.com>
2026-04-22 02:37:14 +0200

    Refactor main.cpp to include GameManagement class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include"GameManagement.h"
+#include <conio.h>
 using namespace std;
 
-int main() {
-    cout << "Hello World";
+int main()
+{
+GameManagement game;
+game.runGame();
+
     return 0;
 }
